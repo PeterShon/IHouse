ba2b1171100d048e5a888ac0b2a5d61471a19684
PeterShon <petershonpublic@gmail.com>
2020-12-24 15:08:27 +0300

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+import $ from '../local_modules/jquery/dist/jquery.min'
+
+//не забудь написать 'export' перед отправляемой функцией
+//import { название функции } from '../js/файл.js'
+
+$(document).ready(() => {
+    // eslint-disable-next-line no-console
+    console.log(`document ready`)
+})
