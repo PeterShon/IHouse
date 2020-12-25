80d02205e7b364075b6b246d03620b33ca35bf3e
PeterShon <petershonpublic@gmail.com>
2020-12-25 17:31:50 +0300

    2020-12-25__svg sprite

--- gulp/tasks/svgSprite.js
+++ gulp/tasks/svgSprite.js
@@ -1,12 +1,23 @@
 const gulp = require('gulp')
 const svgstore = require('gulp-svgstore')
 const rename = require('gulp-rename')
+const cheerio = require('gulp-cheerio')
+const replace = require('gulp-replace')
 
 module.exports = function svgSprite() {
-  return gulp.src('source/img/sprite/*.svg')
+  return gulp.src('src/img/sprite/*.svg')
     .pipe(svgstore({
       inlineSvg: true
     }))
+    .pipe(cheerio({
+      run: function ($) {
+        $('[fill]').removeAttr('fill');
+        $('[stroke]').removeAttr('stroke');
+        $('[style]').removeAttr('style');
+      },
+      parserOptions: { xmlMode: true }
+    }))
+    .pipe(replace('&gt;', '>'))
     .pipe(rename('sprite.svg'))
     .pipe(gulp.dest('docs/img'))
 }
