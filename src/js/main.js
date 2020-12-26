5a1210890fde024b9b73ce5edd0cea95a3e7f482
PeterShon <petershonpublic@gmail.com>
2020-12-26 18:25:02 +0300

    2020-12-26__section intro complete

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,11 @@
 import { moveUp } from '../js/move-up.js'
 import { popups } from '../js/popups.js'
+import { intro } from '../js/intro.js'
 
 $(document).ready(() => {
   moveUp()
   popups()
+  intro()
   // eslint-disable-next-line no-console
 })
 
