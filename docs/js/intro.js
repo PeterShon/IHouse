44908221f25fa5574c1552f1fa128973978e8b6d
PeterShon <petershonpublic@gmail.com>
2020-12-26 23:01:47 +0300

    2020-26-12__parallax fix

--- docs/js/intro.js
+++ docs/js/intro.js
@@ -11,11 +11,11 @@ export function intro() {
     let might_2 = might * 1
     bg.style.background =
       `
-    url(../img/intro-el1.png) calc(-45% + ${posX / might_1}px) calc(135% + ${posY / might_1}px)/57% no-repeat,
-    url(../img/intro-el4.png) calc(112% + ${posX / might_1}px) calc(73% + ${posY / might_1}px)/32% no-repeat,
-    url(../img/intro-el2.png) calc(38.5% + ${posX / might_2}px) calc(20% + ${posY / might_1}px)/15% no-repeat,
-    url(../img/intro-el3.png) calc(66.5% + ${posX / might_2}px) calc(29% + ${posY / might_1}px)/18% no-repeat,
-    url(../img/intro-bg.jpg) center/cover no-repeat
+    url(img/intro-el1.png) calc(-45% + ${posX / might_1}px) calc(135% + ${posY / might_1}px)/57% no-repeat,
+    url(img/intro-el4.png) calc(112% + ${posX / might_1}px) calc(73% + ${posY / might_1}px)/32% no-repeat,
+    url(img/intro-el2.png) calc(38.5% + ${posX / might_2}px) calc(20% + ${posY / might_1}px)/15% no-repeat,
+    url(img/intro-el3.png) calc(66.5% + ${posX / might_2}px) calc(29% + ${posY / might_1}px)/18% no-repeat,
+    url(img/intro-bg.jpg) center/cover no-repeat
     `
   }
   setTimeout(() => {
