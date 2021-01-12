e20981a0b3087d992cec6aacd53e40fc19c8dac1
PeterShon <petershonpublic@gmail.com>
2021-01-12 16:25:01 +0300

    2021-01-12__fix panel

--- src/js/download.js
+++ src/js/download.js
@@ -2,7 +2,6 @@ let body = document.querySelector('body')
 body.classList.add("lock")
 
 window.addEventListener('load', () => {
-  console.log('страница загрузинилась')
   let download = document.querySelector('.download')
   download.classList.add('download_unactive')
   body.classList.remove("lock")
